2ce3383ef33940a6c2681d0242ddc06c4ef6b32b
Pavel Raur <pavel.raur@gmail.com>
2025-03-23 00:17:53 +0100

    Refactoring

--- main_app/tests/tests.py
+++ main_app/tests/tests.py
@@ -19,10 +19,9 @@ def login(username: str, password: str):
 def main():
     token = login('admin', 'admin')
 
+    # Add user
     print('Adding user.')
     input("Press Enter to continue...")
-
-    # Add user
     user1 = httpx.post(
         url='http://localhost:8000/user',
         headers={
@@ -39,12 +38,11 @@ def main():
     )
     user1.raise_for_status()
     user1 = user1.json()
-
     print(json.dumps(user1, indent=4))
-    print('Adding localhost node.')
-    input("Press Enter to continue...")
 
     # Add localhost
+    print('Adding localhost node.')
+    input("Press Enter to continue...")
     localhost_node = httpx.post(
         url='http://localhost:8000/node',
         headers={'Authorization': f'Bearer {token}'},
@@ -55,12 +53,11 @@ def main():
     )
     localhost_node.raise_for_status()
     localhost_node = localhost_node.json()
-
     print(json.dumps(localhost_node, indent=4))
-    print('Adding vm node.')
-    input("Press Enter to continue...")
 
     # Add vm_node
+    print('Adding vm node.')
+    input("Press Enter to continue...")
     vm_node = httpx.post(
         url='http://localhost:8000/node',
         headers={'Authorization': f'Bearer {token}'},
@@ -74,10 +71,9 @@ def main():
 
     print(json.dumps(vm_node, indent=4))
 
+    # Add resource CPU
     print('Adding CPU resource.')
     input("Press Enter to continue...")
-
-    # Add resource CPU
     cpu_resource = httpx.post(
         url='http://localhost:8000/resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -88,12 +84,11 @@ def main():
     )
     cpu_resource.raise_for_status()
     cpu_resource = cpu_resource.json()
-
     print(json.dumps(cpu_resource, indent=4))
-    print('Adding memory resource.')
-    input("Press Enter to continue...")
 
     # Add resource memory
+    print('Adding memory resource.')
+    input("Press Enter to continue...")
     memory_resource = httpx.post(
         url='http://localhost:8000/resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -104,12 +99,11 @@ def main():
     )
     memory_resource.raise_for_status()
     memory_resource = memory_resource.json()
-
     print(json.dumps(memory_resource, indent=4))
-    print('Assigning CPU to localhost.')
-    input("Press Enter to continue...")
 
     # Assign CPU to localhost
+    print('Assigning CPU to localhost.')
+    input("Press Enter to continue...")
     cpu_to_localhost = httpx.post(
         url='http://localhost:8000/node/add_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -121,12 +115,12 @@ def main():
     )
     cpu_to_localhost.raise_for_status()
     cpu_to_localhost = cpu_to_localhost.json()
-
     print(json.dumps(cpu_to_localhost, indent=4))
-    print('Adding CPU notification template.')
-    input("Press Enter to continue...")
+
 
     # Create Notification for CPU
+    print('Adding CPU notification template.')
+    input("Press Enter to continue...")
     cpu_treshold_template = open(
         "../grafana_templates/CPU_Notificaion_template.json",
         "r"
@@ -146,12 +140,12 @@ def main():
     cpu_notification.raise_for_status()
     cpu_notification = cpu_notification.json()
     cpu_treshold_template.close()
-
     print(json.dumps(cpu_notification, indent=4))
-    print('Assigning CPU Notification to All group.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU Notification to All group
+    print('Assigning CPU Notification to All group.')
+    input("Press Enter to continue...")
     cpu_notification_to_all = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -162,12 +156,12 @@ def main():
     )
     cpu_notification_to_all.raise_for_status()
     cpu_notification_to_all = cpu_notification_to_all.json()
-
     print(json.dumps(cpu_notification_to_all, indent=4))
-    print('Adding memory notification template.')
-    input("Press Enter to continue...")
+
 
     # Create Notification for memory
+    print('Adding memory notification template.')
+    input("Press Enter to continue...")
     memory_treshold_template = open(
         "../grafana_templates/Memory_Notification_template.json",
         "r"
@@ -187,12 +181,12 @@ def main():
     memory_notification.raise_for_status()
     memory_notification = memory_notification.json()
     memory_treshold_template.close()
-
     print(json.dumps(memory_notification, indent=4))
-    print('Assigning Memory Notification to All group.')
-    input("Press Enter to continue...")
+
 
     # Assign Memory Notification to All group
+    print('Assigning Memory Notification to All group.')
+    input("Press Enter to continue...")
     memory_notification_to_all = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -203,12 +197,12 @@ def main():
     )
     memory_notification_to_all.raise_for_status()
     memory_notification_to_all = memory_notification_to_all.json()
-
     print(json.dumps(memory_notification_to_all, indent=4))
-    print('Assigning memory to localhost.')
-    input("Press Enter to continue...")
+
 
     # Assign memory to localhost
+    print('Assigning memory to localhost.')
+    input("Press Enter to continue...")
     memory_to_localhost = httpx.post(
         url='http://localhost:8000/node/add_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -220,12 +214,11 @@ def main():
     )
     memory_to_localhost.raise_for_status()
     memory_to_localhost = memory_to_localhost.json()
-
     print(json.dumps(memory_to_localhost, indent=4))
-    print('Removing CPU from localhost.')
-    input("Press Enter to continue...")
 
     # Remove CPU from localhost
+    print('Removing CPU from localhost.')
+    input("Press Enter to continue...")
     cpu_from_localhost = httpx.post(
         url=f'http://localhost:8000/node/remove_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -237,12 +230,12 @@ def main():
     )
     cpu_from_localhost.raise_for_status()
     cpu_from_localhost = cpu_from_localhost.json()
-
     print(json.dumps(cpu_from_localhost, indent=4))
-    print('Adding CPU resource to vm node.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU to vm_node
+    print('Adding CPU resource to vm node.')
+    input("Press Enter to continue...")
     cpu_to_vm_node = httpx.post(
         url='http://localhost:8000/node/add_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -254,12 +247,12 @@ def main():
     )
     cpu_to_vm_node.raise_for_status()
     cpu_to_vm_node = cpu_to_vm_node.json()
-
     print(json.dumps(cpu_to_vm_node, indent=4))
-    print('Removing CPU Notification from All group.')
-    input("Press Enter to continue...")
+
 
     # Remove CPU Notification from All group
+    print('Removing CPU Notification from All group.')
+    input("Press Enter to continue...")
     cpu_notification_from_all = httpx.post(
         url=f'http://localhost:8000/notification/unassign',
         headers={'Authorization': f'Bearer {token}'},
@@ -270,12 +263,12 @@ def main():
     )
     cpu_notification_from_all.raise_for_status()
     cpu_notification_from_all = cpu_notification_from_all.json()
-
     print(json.dumps(cpu_notification_from_all, indent=4))
-    print('Assigning CPU Notification to user1.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU Notification to user1
+    print('Assigning CPU Notification to user1.')
+    input("Press Enter to continue...")
     cpu_notification_to_user1 = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -286,12 +279,12 @@ def main():
     )
     cpu_notification_to_user1.raise_for_status()
     cpu_notification_to_user1 = cpu_notification_to_user1.json()
-
     print(json.dumps(cpu_notification_to_user1, indent=4))
-    print('Removing CPU from vm node.')
-    input("Press Enter to continue...")
+
 
     # Remove CPU from vm_node
+    print('Removing CPU from vm node.')
+    input("Press Enter to continue...")
     cpu_from_vm_node = httpx.post(
         url=f'http://localhost:8000/node/remove_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -303,12 +296,12 @@ def main():
     )
     cpu_from_vm_node.raise_for_status()
     cpu_from_vm_node = cpu_from_vm_node.json()
-
     print(json.dumps(cpu_from_vm_node, indent=4))
-    print('Removing CPU Notification from user1.')
-    input("Press Enter to continue...")
+
 
     # Remove CPU Notification from user1
+    print('Removing CPU Notification from user1.')
+    input("Press Enter to continue...")
     cpu_notification_from_user1 = httpx.post(
         url=f'http://localhost:8000/notification/unassign',
         headers={'Authorization': f'Bearer {token}'},
@@ -319,12 +312,12 @@ def main():
     )
     cpu_notification_from_user1.raise_for_status()
     cpu_notification_from_user1 = cpu_notification_from_user1.json()
-
     print(json.dumps(cpu_notification_from_user1, indent=4))
-    print('Assigning CPU Notification to All group.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU Notification to All group
+    print('Assigning CPU Notification to All group.')
+    input("Press Enter to continue...")
     cpu_notification_to_all = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -335,12 +328,12 @@ def main():
     )
     cpu_notification_to_all.raise_for_status()
     cpu_notification_to_all = cpu_notification_to_all.json()
-
     print(json.dumps(cpu_notification_to_all, indent=4))
-    print('Adding test user.')
-    input("Press Enter to continue...")
+
 
     # Add user
+    print('Adding test user.')
+    input("Press Enter to continue...")
     test_user = httpx.post(
         url='http://localhost:8000/user',
         headers={'Authorization': f'Bearer {token}'},
@@ -355,12 +348,12 @@ def main():
     )
     test_user.raise_for_status()
     test_user = test_user.json()
-
     print(json.dumps(test_user, indent=4))
-    print('Assigning CPU to vm node.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU to vm_node
+    print('Assigning CPU to vm node.')
+    input("Press Enter to continue...")
     cpu_to_vm_node = httpx.post(
         url='http://localhost:8000/node/add_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -372,12 +365,12 @@ def main():
     )
     cpu_to_vm_node.raise_for_status()
     cpu_to_vm_node = cpu_to_vm_node.json()
-
     print(json.dumps(cpu_to_vm_node, indent=4))
-    print('Creating user subgroup.')
-    input("Press Enter to continue...")
+
 
     # Create Subgroup for user
+    print('Creating user subgroup.')
+    input("Press Enter to continue...")
     subgroup = httpx.post(
         url='http://localhost:8000/group',
         headers={'Authorization': f'Bearer {token}'},
@@ -388,12 +381,12 @@ def main():
     )
     subgroup.raise_for_status()
     subgroup = subgroup.json()
-
     print(json.dumps(subgroup, indent=4))
-    print('Assigning CPU Notification to subgroup.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU Notification to subgroup
+    print('Assigning CPU Notification to subgroup.')
+    input("Press Enter to continue...")
     cpu_notification_to_subgroup = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -404,12 +397,12 @@ def main():
     )
     cpu_notification_to_subgroup.raise_for_status()
     cpu_notification_to_subgroup = cpu_notification_to_subgroup.json()
-
     print(json.dumps(cpu_notification_to_subgroup, indent=4))
-    print('Removing CPU Notification from All group.')
-    input("Press Enter to continue...")
+
 
     # Remove CPU Notification from All group
+    print('Removing CPU Notification from All group.')
+    input("Press Enter to continue...")
     cpu_notification_from_all = httpx.post(
         url=f'http://localhost:8000/notification/unassign',
         headers={'Authorization': f'Bearer {token}'},
@@ -420,12 +413,12 @@ def main():
     )
     cpu_notification_from_all.raise_for_status()
     cpu_notification_from_all = cpu_notification_from_all.json()
-
     print(json.dumps(cpu_notification_from_all, indent=4))
-    print('Moving user1 to subgroup.')
-    input("Press Enter to continue...")
+
 
     # Move user1 to subgroup
+    print('Moving user1 to subgroup.')
+    input("Press Enter to continue...")
     user1_to_subgroup = httpx.post(
         url='http://localhost:8000/group/add_user',
         headers={'Authorization': f'Bearer {token}'},
@@ -436,24 +429,24 @@ def main():
     )
     user1_to_subgroup.raise_for_status()
     user1_to_subgroup = user1_to_subgroup.json()
-
     print(json.dumps(user1_to_subgroup, indent=4))
-    print('Removing Subgroup.')
-    input("Press Enter to continue...")
+
 
     # Remove Subgroup
+    print('Removing Subgroup.')
+    input("Press Enter to continue...")
     subgroup_rm = httpx.delete(
         url=f'http://localhost:8000/group/{subgroup["id"]}',
         headers={'Authorization': f'Bearer {token}'}
     )
     subgroup_rm.raise_for_status()
     subgroup_rm = subgroup_rm.json()
-
     print(json.dumps(subgroup_rm, indent=4))
-    print('Assigning CPU Notification to All group.')
-    input("Press Enter to continue...")
+
 
     # Reasign CPU Notification to All group
+    print('Assigning CPU Notification to All group.')
+    input("Press Enter to continue...")
     cpu_notification_to_all = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -464,48 +457,48 @@ def main():
     )
     cpu_notification_to_all.raise_for_status()
     cpu_notification_to_all = cpu_notification_to_all.json()
-
     print(json.dumps(cpu_notification_to_all, indent=4))
-    print('Removing test user.')
-    input("Press Enter to continue...")
+
 
     # Remove user
+    print('Removing test user.')
+    input("Press Enter to continue...")
     test_user_rm = httpx.delete(
         url=f'http://localhost:8000/user/{test_user["id"]}',
         headers={'Authorization': f'Bearer {token}'}
     )
     test_user_rm.raise_for_status()
     test_user_rm = test_user_rm.json()
-
     print(json.dumps(test_user_rm, indent=4))
-    print('Removing localhost node.')
-    input("Press Enter to continue...")
+
 
     # Remove localhost
+    print('Removing localhost node.')
+    input("Press Enter to continue...")
     node1_rm = httpx.delete(
         url=f'http://localhost:8000/node/{localhost_node["id"]}',
         headers={'Authorization': f'Bearer {token}'}
     )
     node1_rm.raise_for_status()
     node1_rm = node1_rm.json()
-
     print(json.dumps(node1_rm, indent=4))
-    print('Removing CPU Notification.')
-    input("Press Enter to continue...")
+
 
     # Remove CPU Notification
+    print('Removing CPU Notification.')
+    input("Press Enter to continue...")
     cpu_notification_rm = httpx.delete(
         url=f'http://localhost:8000/notification/{cpu_notification["id"]}',
         headers={'Authorization': f'Bearer {token}'}
     )
     cpu_notification_rm.raise_for_status()
     cpu_notification_rm = cpu_notification_rm.json()
-
     print(json.dumps(cpu_notification_rm, indent=4))
-    print('Assigning memory to localhost.')
-    input("Press Enter to continue...")
+
 
     # Assign memory to vm node
+    print('Assigning memory to localhost.')
+    input("Press Enter to continue...")
     memory_to_localhost = httpx.post(
         url='http://localhost:8000/node/add_resource',
         headers={'Authorization': f'Bearer {token}'},
@@ -517,24 +510,24 @@ def main():
     )
     memory_to_localhost.raise_for_status()
     memory_to_localhost = memory_to_localhost.json()
-
     print(json.dumps(memory_to_localhost, indent=4))
-    print('Removing Memory Resource.')
-    input("Press Enter to continue...")
+
 
     # Remove Memory Resource
+    print('Removing Memory Resource.')
+    input("Press Enter to continue...")
     memory_resource_rm = httpx.delete(
         url=f'http://localhost:8000/resource/{memory_resource["id"]}',
         headers={'Authorization': f'Bearer {token}'}
     )
     memory_resource_rm.raise_for_status()
     memory_resource_rm = memory_resource_rm.json()
-
     print(json.dumps(memory_resource_rm, indent=4))
-    print('Adding Notification for CPU again.')
-    input("Press Enter to continue...")
+
 
     # Add Notification for CPU again
+    print('Adding Notification for CPU again.')
+    input("Press Enter to continue...")
     cpu_treshold_template = open(
         "../grafana_templates/CPU_Notificaion_template.json",
         "r"
@@ -554,12 +547,12 @@ def main():
     cpu_notification.raise_for_status()
     cpu_notification = cpu_notification.json()
     cpu_treshold_template.close()
-
     print(json.dumps(cpu_notification, indent=4))
-    print('Assigning CPU Notification to All group.')
-    input("Press Enter to continue...")
+
 
     # Assign CPU Notification to All group
+    print('Assigning CPU Notification to All group.')
+    input("Press Enter to continue...")
     cpu_notification_to_all = httpx.post(
         url='http://localhost:8000/notification/assign',
         headers={'Authorization': f'Bearer {token}'},
@@ -570,14 +563,14 @@ def main():
     )
     cpu_notification_to_all.raise_for_status()
     cpu_notification_to_all = cpu_notification_to_all.json()
-
     print(json.dumps(cpu_notification_to_all, indent=4))
+
+
+    # Assign CPU Notification to CPU resource
     print('Assigning CPU Notification to CPU resource.')
     input("Press Enter to continue...")
-
     cpu_notification_assign = cpu_notification_to_all.copy()
     cpu_notification_assign['resource_id'] = cpu_resource['id']
-    # Assign CPU Notification to CPU resource
     cpu_notification_to_resource = httpx.put(
         url='http://localhost:8000/notification',
         headers={'Authorization': f'Bearer {token}'},
@@ -585,8 +578,8 @@ def main():
     )
     cpu_notification_to_resource.raise_for_status()
     cpu_notification_to_resource = cpu_notification_to_resource.json()
-
     print(json.dumps(cpu_notification_to_resource, indent=4))
+
 
     # TODO - add time based notifications
 
